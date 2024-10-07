c530d3179d61d273619f7e8a206b2e55f51053a9
lightworld689 <107845122+lightworld689@users.noreply.github.com>
2024-10-07 12:28:38 +0800

    feat: change rl check

--- main.py
+++ main.py
@@ -124,9 +124,6 @@ async def join_channel(nick, password, channel, ws_link):
 
                 if message.get("cmd") == "warn" and "You are sending too much text. Wait a moment and try again." in message.get("text", ""):
                     break
-
-                if message.get("cmd") == "warn" and "You already have that name" not in message.get("text", ""):
-                    break
                 
                 if message.get("cmd") == "info" and message.get("type") == "whisper":
                     from_user = message.get("from")
